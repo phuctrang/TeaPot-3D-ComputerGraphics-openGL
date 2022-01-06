289c2bf4f9c609e05fad9d84888f87618462c3cc
Hồ Phúc Trang <56389219+phuctrang@users.noreply.github.com>
2022-01-06 15:28:55 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,19 +3,21 @@
 #include<stdio.h>
 #include <stdarg.h>
 #include <math.h>
+#include <windows.h>
 #define GL_GLEXT_PROTOTYPES
+
 GLuint teapotList;
 float g_x = 0.0;
 float g_z = 0.0;
 float lz = -10.0;
 float lx = 0.0;
-float angle = 0.0;
+float angle = 0.0; 
  
 float deltaAngle = 0.0f;
 int xOrigin = -1;
 bool g_is_rotate = false;
 
-// c�c toa do
+// các tọa độ ấm trà
 long patchdata[][16] =
 {
 	{102, 103, 104, 105, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
@@ -23,16 +25,15 @@ long patchdata[][16] =
 	{24, 25, 26, 27, 29, 30, 31, 32, 33, 34, 35, 36,37, 38, 39, 40},
 	{96, 96, 96, 96, 97, 98, 99, 100, 101, 101, 101,101, 0, 1, 2, 3,},
 	{0, 1, 2, 3, 106, 107, 108, 109, 110, 111, 112,113, 114, 115, 116, 117},
-	     /* bottom */
+	     /* đế ấm */
 	{118, 118, 118, 118, 124, 122, 119, 121, 123, 126,125, 120, 40, 39, 38, 37},
-	     /* handle */
+	     /* tay cầm */
 	{41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52,53, 54, 55, 56},
 	{53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64,28, 65, 66, 67},
-	     /* spout */
+	     /* ngòi ấm */
 	{68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,80, 81, 82, 83},
 	{80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91,92, 93, 94, 95}
 };
-
 
 float cpdata[][3] =
 {
@@ -89,7 +90,6 @@ float ver[8][3] =
     {0.05,0.05,-0.05},
     {0.05,-0.05,-0.05},
 };
-
 GLfloat color[8][3] = 
 {
     {0.0,0.0,0.0},
@@ -125,8 +125,6 @@ void Init()
   glMateriali(GL_FRONT, GL_SHININESS, shininess);
  
 }
-
-
 static float tex[2][2][2] =
 {
    { {0, 0},
@@ -134,7 +132,6 @@ static float tex[2][2][2] =
    { {0, 1},
      {1, 1}}
 };
-
 void quad(int a,int b,int c,int d)
 {
     glBegin(GL_QUADS);
@@ -161,7 +158,7 @@ void colorcube()
     quad(0,1,5,4);
 }
 
-//ve am
+//Hàm vẽ ấm trà
 static void teapot(GLint grid, GLdouble scale, GLenum type)
 {
    float p[4][4][3], q[4][4][3], r[4][4][3], s[4][4][3];
@@ -224,42 +221,37 @@ void glutSolidTeapot1(GLdouble scale)
 {
    teapot(14, scale, GL_FILL);
 }
- //mat do cua net
+
 void Tea(GLdouble scale)
 {
-	//teapot(10, scale, GL_LINE);
-	teapot(90, scale, GL_LINE);
+	teapot(100, scale, GL_LINE);
 }
 
-
-// de ban
+// Đế bàn trà
 void wall(double thickness)
 {
-    /////
     glPushMatrix();
     glTranslated(0.5,0.5*thickness,0.5);
-    glScaled(1.0,thickness,1.0);
-    glutSolidCube(1.0);
+    glScaled(3.0,thickness,3.0);
+    glutSolidCube(2.0);
     glPopMatrix();
 }
-// chan ban
+// Chân bàn
 void tableLeg(double thick,double len)
 {
-	////
     glPushMatrix();
     glTranslated(0,len/2,0);
     glScaled(thick,len,thick);
     glutSolidCube(1.0);
     glPopMatrix();
 }
+// Bàn
 void table(double topWid,double topThick,double legThick,double legLen)
 {
-	
-	////
     glPushMatrix();
     glTranslated(0,legLen,0);
     glScaled(topWid,topThick,topWid);
-    glutSolidCube(1.0);
+    glutSolidCube(1.5);
     glPopMatrix();
     double dist=0.95*topWid/2.0-legThick/2.0;
     glPushMatrix();
@@ -273,7 +265,6 @@ void table(double topWid,double topThick,double legThick,double legLen)
     tableLeg(legThick,legLen);
     glPopMatrix();
 }
-
 void ReShape(int width, int height)
 {
   glViewport(0, 0, width, height);
@@ -283,7 +274,6 @@ void ReShape(int width, int height)
   gluPerspective(45.0, ratio, 1, 100.0);
   glMatrixMode(GL_MODELVIEW);
 }
- 
 void OnKeyDown(int key, int xx, int yy) {
   switch (key)
   {
@@ -301,7 +291,6 @@ void OnKeyDown(int key, int xx, int yy) {
     break;
   }
 }
- 
 void mouseButton(int button, int state, int x, int y)
 {
   // only start motion if the left button is pressed
@@ -322,110 +311,314 @@ void mouseButton(int button, int state, int x, int y)
     }
   }
 }
- 
 void mouseMove(int x, int y)
 {
   if (g_is_rotate)
   {
-    // this will only be true when the left button is down
     deltaAngle += (x - xOrigin) * 0.0005f;
-    // update camera's direction
     g_x = 10 * sin(angle + deltaAngle);
     g_z = 10 * cos(angle + deltaAngle);
   }
- 
 }
- 
- 
+double rotate_y = 0;                              		
+double rotate_x = 0;
 void DrawCoordinate()
 {
   glDisable(GL_LIGHTING);
-  glBegin(GL_LINES);
+  //glBegin(GL_LINES);
   glColor3f(1.0, 0.0, 0.0);
   glVertex3f(-100.0, 0.0, 0.0);
   glVertex3f(100.0, 0.0, 0.0);
   glEnd();
-  glBegin(GL_LINES);
+  //glBegin(GL_LINES);
   glColor3f(0.0, 1.0, 0.0);
   glVertex3f(0.0, -100.0, 0.0);
   glVertex3f(0.0, 100.0, 0.0);
   glEnd();
-  glBegin(GL_LINES);
+  //glBegin(GL_LINES);
   glColor3f(0.0, 0.0, 1.0);
   glVertex3f(0.0, 0.0, -100.0);
   glVertex3f(0.0, 0.0, 100.0);
   glEnd();
+	// Cây cắm hoa
+	glBegin(GL_POLYGON);	
+   	glColor4f( 1.0 , 1.0, 1.0, 0.3);
+	glVertex3f(-0.01 , 0.8, 0.0);
+        glVertex3f(0.01, 0.8, 0.0);
+	glVertex3f(0.01,-0.69, 0.0);
+	glVertex3f(-0.01,-0.69, 0.0);
+	glEnd();
+	
+	glBegin(GL_POLYGON);	
+   	glColor4f( 1.0 , 1.0, 1.0, 0.3);
+	glVertex3f(-0.21 , 0.75, 0.0);
+	glVertex3f(-0.19 , 0.75, 0.0);
+       	glVertex3f(0.0 , 0.0, 0.0);
+	glEnd();
+
+	glBegin(GL_POLYGON);	
+   	glColor4f( 1.0 , 1.0, 1.0, 0.3);
+	glVertex3f( 0.28 , 0.75, 0.0);
+	glVertex3f( 0.31 , 0.75, 0.0);
+       	glVertex3f(0.0 , 0.2, 0.0);
+	glEnd();
+
+	glBegin(GL_POLYGON);	
+   	glColor4f( 1.0 , 1.0, 1.0, 0.3);
+	glVertex3f(-0.21 , 0.75, 0.0);
+	glVertex3f(-0.19 , 0.75, 0.0);
+       	glVertex3f(0.0 , 0.0, 0.0);
+	glEnd();
+	
+   glBegin(GL_POLYGON);						//Front-T
+   glColor4f(1.0, 0.0, 0.0, 0.3	);
+   glVertex3f(-0.05,0.05, -0.3);
+   glVertex3f(-0.3,0.5, -0.2);
+   glVertex3f(0.3,0.5, -0.2);
+   glVertex3f(0.05,0.05, -0.3);
+   glEnd();
+
+   glBegin(GL_POLYGON);						//Front-B
+   glColor4f( 1.0, 0.0, 0.0, 0.6);
+   glVertex3f(-0.3,-0.7, -0.2);
+   glVertex3f(-0.05, -0.05, -0.3);
+   glVertex3f(0.05,-0.05, -0.3);
+   glVertex3f(0.3,-0.7, -0.2);
+   glEnd();
+
+   glBegin(GL_POLYGON);						//Front-R
+   glColor4f( 0.76, 0.0, 0.0, 0.6);
+   glVertex3f(0.05, 0.05, -0.3);
+   glVertex3f(0.3, 0.5, -0.2);
+   glVertex3f(0.3,-0.7, -0.2);
+   glVertex3f(0.05,-0.05, -0.3);
+   glEnd();
+
+   glBegin(GL_POLYGON);		
+   glColor4f( 0.76, 0.0, 0.0, 0.6);				//Front-L
+   glVertex3f(-0.3,0.5, -0.2);
+   glVertex3f(-0.05,0.05, -0.3);
+   glVertex3f(-0.05,-0.05, -0.3);
+   glVertex3f(-0.3,-0.7, -0.2);
+   glEnd();
+
+   glBegin(GL_POLYGON);						//RIGHT-T
+   glColor4f( 1.0, 0.0, 0.0, 0.3);
+   glVertex3f(0.4,0.05, -0.05);
+   glVertex3f(0.3,0.5, -0.2);
+   glVertex3f(0.3,0.5, 0.2);
+   glVertex3f(0.4,0.05, 0.05);
+   glEnd();
+
+   glBegin(GL_POLYGON);						//RIGHT-B
+   glColor4f( 1.0, 0.0, 0.0, 0.6);
+   glVertex3f(0.3,-0.7, -0.2);
+   glVertex3f(0.4,-0.05, -0.05);
+   glVertex3f(0.4,-0.05, 0.05);
+   glVertex3f(0.3,-0.7, 0.2);
+   glEnd();
+
+   glBegin(GL_POLYGON);						//RIGHT-R
+   glColor4f( 0.76, 0.0, 0.0, 0.6);
+   glVertex3f(0.4,0.05, 0.05);
+   glVertex3f(0.3,0.5, 0.2);
+   glVertex3f(0.3,-0.7, 0.2);
+   glVertex3f(0.4,-0.05, 0.05);
+   glEnd();
+
+   glBegin(GL_POLYGON);		
+   glColor4f( 0.76, 0.0, 0.0, 0.6);				//RIGHT-L
+   glVertex3f(0.3,0.5, -0.2);
+   glVertex3f(0.4,0.05, -0.05);
+   glVertex3f(0.4,-0.05, -0.05);
+   glVertex3f(0.3,-0.7, -0.2);
+   glEnd();
+
+   glBegin(GL_POLYGON);						//BACK-T
+   glColor4f( 1.0, 0.0, 0.0, 0.3);
+   glVertex3f(-0.05,0.05, 0.3);
+   glVertex3f(-0.3,0.5, 0.2);
+   glVertex3f(0.3,0.5, 0.2);
+   glVertex3f(0.05,0.05, 0.3);
+   glEnd();
+
+   glBegin(GL_POLYGON);						//BACK-B
+   glColor4f( 1.0, 0.0, 0.0, 0.6);
+   glVertex3f(-0.3,-0.7, 0.2);
+   glVertex3f(-0.05, -0.05, 0.3);
+   glVertex3f(0.05,-0.05, 0.3);
+   glVertex3f(0.3,-0.7, 0.2);
+   glEnd();
+
+   glBegin(GL_POLYGON);						//BACK-R
+   glColor4f( 0.76, 0.0, 0.0, 0.6);
+   glVertex3f(0.05, 0.05, 0.3);
+   glVertex3f(0.3, 0.5, 0.2);
+   glVertex3f(0.3,-0.7, 0.2);
+   glVertex3f(0.05,-0.05, 0.3);
+   glEnd();
+
+   glBegin(GL_POLYGON);		
+   glColor4f( 0.76, 0.0, 0.0, 0.6);				//BACK-L
+   glVertex3f(-0.3,0.5, 0.2);
+   glVertex3f(-0.05,0.05, 0.3);
+   glVertex3f(-0.05,-0.05, 0.3);
+   glVertex3f(-0.3,-0.7, 0.2);
+   glEnd();
+
+
+   glBegin(GL_POLYGON);						//LEFT-T
+   glColor4f( 1.0, 0.0, 0.0, 0.3);
+   glVertex3f(-0.4,0.05, -0.05);
+   glVertex3f(-0.3,0.5, -0.2);
+   glVertex3f(-0.3,0.5, 0.2);
+   glVertex3f(-0.4,0.05, 0.05);
+   glEnd();
+
+   glBegin(GL_POLYGON);						//LEFT-B
+   glColor4f( 1.0, 0.0, 0.0, 0.6);
+   glVertex3f(-0.3,-0.7, -0.2);
+   glVertex3f(-0.4,-0.05, -0.05);
+   glVertex3f(-0.4,-0.05, 0.05);
+   glVertex3f(-0.3,-0.7, 0.2);
+   glEnd();
+
+   glBegin(GL_POLYGON);						//LEFT-R
+   glColor4f( 0.76, 0.0, 0.0, 0.6);
+   glVertex3f(-0.4,0.05, 0.05);
+   glVertex3f(-0.3,0.5, 0.2);
+   glVertex3f(-0.3,-0.7, 0.2);
+   glVertex3f(-0.4,-0.05, 0.05);
+   glEnd();
+
+   glBegin(GL_POLYGON);		
+   glColor4f( 0.76, 0.0, 0.0, 0.6);				//LEFT-L
+   glVertex3f(-0.3,0.5, -0.2);
+   glVertex3f(-0.4,0.05, -0.05);
+   glVertex3f(-0.4,-0.05, -0.05);
+   glVertex3f(-0.3,-0.7, -0.2);
+   glEnd();
+
+   glBegin(GL_POLYGON);	
+   glColor4f( 0.4, 0.13, 0.245, 1.0);	   				//BASE
+   glVertex3f(-0.3,-0.7, -0.2);
+   glVertex3f(-0.3,-0.7, 0.2);
+   glVertex3f(0.3,-0.7, 0.2);
+   glVertex3f(0.3,-0.7, -0.2);
+   glEnd();
+        // Flower one ---
+        glColor3f(0.1, 0.72, 0.32);
+        glPushMatrix();
+        glTranslatef(0.25,0.7,0.0);
+	glutSolidSphere(0.07,30,30);
+        glPopMatrix();
+    
+	glColor3f(0.34, 0.21, 0.52);
+        glPushMatrix();
+        glTranslatef(0.3,0.76,0.0);
+	glutSolidSphere(0.03,30,30);
+        glPopMatrix();
+
+        glPushMatrix();
+        glTranslatef(0.2,0.76,0.0);
+	glutSolidSphere(0.03,30,30);
+        glPopMatrix();
+
+        glPushMatrix();
+        glTranslatef(0.25,0.76,0.0);
+	glutSolidSphere(0.03,30,30);
+        glPopMatrix();
+ 								//Flower two
+        glBegin(GL_POLYGON);	
+   	glColor4f( 0.35, 0.13, 0.245, 1.0);	   				
+	glVertex3f(-0.35,0.75,0.0);
+	glVertex3f(-0.3,0.8,-0.02);
+	glVertex3f(-0.2,0.75,0.0);
+	glVertex3f(-0.3,0.7,-0.02);
+   	glEnd();
+
+        glBegin(GL_POLYGON);
+   	glColor4f( 0.4, 0.13, 0.245, 1.0);	   				
+	glVertex3f(-0.2,0.75,0.0);
+	glVertex3f(-0.1,0.8,-0.02);
+	glVertex3f(-0.05,0.75,0.0);
+	glVertex3f(-0.1,0.7,-0.02);
+   	glEnd();
+
+        glBegin(GL_POLYGON);
+   	glColor4f( 0.45, 0.13, 0.245, 1.0);	   				
+	glVertex3f(-0.2,0.75,0.0);
+	glVertex3f(-0.25,0.85,-0.02);
+	glVertex3f(-0.2,0.9,0.0);
+	glVertex3f(-0.15,0.85,-0.02);
+   	glEnd();
+
+	glColor4f(0.0, 0.72, 0.32,0.2);
+        glPushMatrix();
+        glTranslatef(0.0,0.9,0.0);
+	glutSolidSphere(0.1,90,90);
+        glPopMatrix();
+	
   glEnable(GL_LIGHTING);
 }
-
-
-
-// display
+// Hàm hiển thị
 void RenderScene()
 {
   glClear(GL_COLOR_BUFFER_BIT  | GL_DEPTH_BUFFER_BIT);
-  
   glLoadIdentity();
-
   gluLookAt
   	(g_x, 1.0, g_z, 
 	  0, 1.0, 0, 
 	  0, 1, 0);
-  DrawCoordinate();
-  
+	
+	DrawCoordinate();
+	glTranslated(0.0,-1.5,0.1);
+	
   glPushMatrix();
     glTranslated(0.4,0.4,0.6);
     glRotated(45,0,0,1);
     glScaled(0.08,0.08,0.08);
     glPopMatrix();
     glPushMatrix();
-  	glTranslated(0.5,0.38,0.5);
+  	glTranslated(0.8,1.2,0.8);
     glRotated(20,0,1,0);
-    //
-  Tea(0.1);
+	
+  Tea(0.4);
   glPopMatrix();
-  //////////////////////////////////////////
   glPushMatrix();
   glTranslated(0.25,0.42,0.35);
     glPopMatrix();
     glPushMatrix();
     glTranslated(0.5,0,0.5);
-  table(0.6,0.02,0.02,0.3);
+  table(1.5,0.06,0.06,0.8);
   
   glPopMatrix();
-  wall(0.02);
+  wall(0.08);
   glPushMatrix();
-  glRotated(90.0,0.0,0.0,1.0);    //draw second wall after rotating x axis by 90degre
- ////////////////////////
-  //wall(0.02);
+  glRotated(90.0,0.0,0.0,1.0);    
+	
   glPopMatrix();
 
   glPushMatrix();
-  glRotated(-90.0,1.0,0.0,0.0);   //draw floor
- ////////////////////////////////
-  //wall(0.02);
+  glRotated(-90.0,1.0,0.0,0.0);  
+	
   glPopMatrix();
-  /////////////////////////////////
    glPushMatrix();
    
   	glTranslated(0.3,0.38,0.5);
     glRotated(30,0,1,0);
-  	//colorcube();
-  	//glColor3f(0.0,1.0,0.0);
+
      glPopMatrix();
-     //////////////////////
+
      glFlush();
 }
-
-
-
 int main()
 {
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
   
   glutInitWindowSize(640, 480);
   glutInitWindowPosition(100, 100);
-  glutCreateWindow("ve ban am");
+  glutCreateWindow("Ban tra 3D su dung Opengl");
   Init();
   glutReshapeFunc(ReShape);
   glutDisplayFunc(RenderScene);
@@ -433,7 +626,6 @@ int main()
   glutSpecialFunc(OnKeyDown);
   glutMouseFunc(mouseButton);
   glutMotionFunc(mouseMove);
-
   glutMainLoop();
  
   return 0;
